ab641b54fdb4eb793a9042bbbad14299b435fafe
wendhelclaudio-lang <wendhelclaudio@gmail.com>
2026-03-05 19:38:15 -0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,46 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
+import { 
+getAuth,
+signInWithEmailAndPassword,
+createUserWithEmailAndPassword,
+onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+const firebaseConfig = {
+apiKey: "SUA_API_KEY",
+authDomain: "SEU_PROJETO.firebaseapp.com",
+projectId: "SEU_PROJETO",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+window.login = async () => {
+
+const email = document.getElementById("email").value;
+const senha = document.getElementById("senha").value;
+
+await signInWithEmailAndPassword(auth,email,senha);
+
+}
+
+window.criarConta = async () => {
+
+const email = document.getElementById("email").value;
+const senha = document.getElementById("senha").value;
+
+await createUserWithEmailAndPassword(auth,email,senha);
+
+}
+
+onAuthStateChanged(auth,user=>{
+
+if(user){
+
+document.getElementById("loginBox").style.display="none";
+document.getElementById("app").style.display="block";
+
+}
+
+});
