c5184ca507023bbed1a9e6d332586fed4f2d61fd
wendhelclaudio-lang <wendhelclaudio@gmail.com>
2026-03-05 19:43:21 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 
-import { 
+import {
 getAuth,
 signInWithEmailAndPassword,
 createUserWithEmailAndPassword,
@@ -8,9 +8,12 @@ onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const firebaseConfig = {
-apiKey: "SUA_API_KEY",
-authDomain: "SEU_PROJETO.firebaseapp.com",
-projectId: "SEU_PROJETO"
+apiKey: "AIzaSyC9c9mUFCvUd3HhbE4ePOFFeb66ZgvYMSc",
+authDomain: "meu-caixa-f130f.firebaseapp.com",
+projectId: "meu-caixa-f130f",
+storageBucket: "meu-caixa-f130f.firebasestorage.app",
+messagingSenderId: "185078430495",
+appId: "1:185078430495:web:2ef34d0a3d8b514cc44e2a"
 };
 
 const app = initializeApp(firebaseConfig);
