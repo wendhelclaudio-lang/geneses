ffe465879a51ca4286c08f7db150c24e559eec02
wendhelclaudio-lang <wendhelclaudio@gmail.com>
2026-03-05 03:16:51 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,35 @@
-self.addEventListener('fetch', function(event) {});
+const CACHE_NAME = 'foton-v1';
+const ASSETS = [
+  '/geneses/',
+  '/geneses/index.html',
+  '/geneses/manifest.json'
+];
+
+// Instalação do Service Worker e Cache dos arquivos essenciais
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Ativação e limpeza de caches antigos
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Estratégia de busca: Tenta a rede, se falhar, usa o cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
