115335a749b5a9949550048b82ea5675f38ff580
wendhelclaudio-lang <wendhelclaudio@gmail.com>
2026-03-05 21:12:15 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,16 +1,10 @@
-// CONFIGURAÇÃO FIREBASE
 const firebaseConfig = {
-  apiKey: "AIzaSyD2TXACQ5t8vIsH2iZLq79oHeUI5y0Wo",
+  apiKey: "AIzaSyD2TXACQ5T8xTsH2jzrLg79oHeHU5y8Wo",
   authDomain: "meucaixa-d8d24.firebaseapp.com",
   projectId: "meucaixa-d8d24",
-  storageBucket: "meucaixa-d8d24.firebasestorage.app",
+  storageBucket: "meucaixa-d8d24.appspot.com",
   messagingSenderId: "486750418195",
   appId: "1:486750418195:web:4c33801663486abda796e6"
 };
 
-// inicializa
 firebase.initializeApp(firebaseConfig);
-
-// auth
-window.auth = firebase.auth();
-window.provider = new firebase.auth.GoogleAuthProvider();
