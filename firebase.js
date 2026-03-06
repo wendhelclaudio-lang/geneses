fe43f83dd7282e6cb2e36c29c33ff01cd85da69b
wendhelclaudio-lang <wendhelclaudio@gmail.com>
2026-03-05 21:45:55 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,10 +1,8 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyD2TXACQ5T8xTsH2jzrLg79oHeHU5y8Wo",
-  authDomain: "meucaixa-d8d24.firebaseapp.com",
-  projectId: "meucaixa-d8d24",
-  storageBucket: "meucaixa-d8d24.appspot.com",
-  messagingSenderId: "486750418195",
-  appId: "1:486750418195:web:4c33801663486abda796e6"
+  apiKey: "AIzaSyC9c9mUFCvUd3HhbE4ePOFFeb66ZgvYMSc",
+  authDomain: "meu-caixa-f130f.firebaseapp.com",
+  projectId: "meu-caixa-f130f",
+  storageBucket: "meu-caixa-f130f.firebasestorage.app",
+  messagingSenderId: "185078430495",
+  appId: "1:185078430495:web:2ef34d0a3d8b514cc44e2a"
 };
-
-firebase.initializeApp(firebaseConfig);
