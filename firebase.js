ca4df311c752a39b7b334433bef14dc8b55920ba
wendhelclaudio-lang <wendhelclaudio@gmail.com>
2026-03-06 10:25:40 -0300

    Delete firebase.js

--- firebase.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyD2TXAC0O5t8vTsH2jzLqZ9QoReHU5y0Wo",
-  authDomain: "meucaixa-d8d24.firebaseapp.com",
-  projectId: "meucaixa-d8d24",
-  storageBucket: "meucaixa-d8d24.firebasestorage.app",
-  messagingSenderId: "486750418195",
-  appId: "1:486750418195:web:4c33801663486abda796e6"
-};
